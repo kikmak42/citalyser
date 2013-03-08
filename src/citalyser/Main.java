ea70806a4016185a43029656f43b352e7fe7424c
anjaykumar93 <anjaykumar93@gmail.com>
2013-03-08 23:42:19 +0530

    Added dummy Main.java

---
+++ src/citalyser/Main.java
@@ -0,0 +1,12 @@
+package citalyser;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("RK Opensoft 2013");
+    }
+}
