b434b112201440deca19f3f79b238b9486249ca9
sahil.sharma <https://sahil_sharma@bitbucket.org>
2013-03-13 23:07:58 +0530

    class queryresult's get and set functions done

--- src/citalyser/api/QueryResult.java
+++ src/citalyser/api/QueryResult.java
@@ -14,6 +14,22 @@ public class QueryResult {
     private int hasauthourList;
     private ArrayList<Author> authorList;
     private PaperCollection paperCollection;
-    
-    
+    public void setHasAuthorList(int i){
+        this.hasauthourList = i;
+    }
+    public void setAuthorList(ArrayList<Author> authorlist){
+        this.authorList = new ArrayList<>(authorlist);
+    }
+    public void setPaperCollection(PaperCollection papercollection){
+        this.paperCollection = papercollection;
+    }
+    public int getHasAuthorList(){
+        return this.hasauthourList;
+    }
+    public ArrayList<Author> getAuthorList(){
+        return this.authorList;
+    }
+    public PaperCollection getPaperCollection(){
+        return this.paperCollection;
+    }
 }
