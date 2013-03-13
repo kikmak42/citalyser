8384dda76ef95904d578027a68953b7252ee506b
sahil.sharma <https://sahil_sharma@bitbucket.org>
2013-03-13 16:45:26 +0530

    added new classes PageresultPaperCollection and queryresult for return type from parser

---
+++ src/citalyser/api/PageResultPaperCollection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citalyser.api;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sahil
+ */
+public class PageResultPaperCollection {
+    private PaperCollection paperCollection;
+    private ArrayList<String> citedByList;
+    
+}
