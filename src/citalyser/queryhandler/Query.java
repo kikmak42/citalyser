7ef2cc5879b5341198aa7773495eebf87120215f
Pujari Rajkumar <rajkumarsaikorian@gmail.com>
2013-03-14 19:41:16 +0530

    queryhandler module added

---
+++ src/citalyser/queryhandler/Query.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citalyser.queryhandler;
+
+/**
+ *
+ * @author rajkumar
+ */
+public class Query{
+    public int flag;
+    public String name;
+    public String ID;
+    public int max_year;
+    public int min_year;
+    public int start_page;
+    public boolean sort_flag;
+    public boolean h_idx;
+    public boolean i_idx;
+}
