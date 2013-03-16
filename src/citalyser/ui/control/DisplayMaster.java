120d1ece8bde4ca8c66335fd03748c43250d3249
sahil.sharma <https://sahil_sharma@bitbucket.org>
2013-03-16 23:38:38 +0530

    dsfgdh

--- src/citalyser/ui/control/DisplayMaster.java
+++ src/citalyser/ui/control/DisplayMaster.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package citalyser.ui.control;
-
+   
 import citalyser.model.PaperCollection;
 import citalyser.ui.control.masters.SearchMaster;
 import citalyser.ui.control.masters.SettingsMaster;
