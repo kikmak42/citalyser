52e578fb1a41376da149911a7e1a8d6186993d59
Kaushik Kumar Mahato <kkm.iitkgp@gmail.com>
2013-03-19 21:34:11 +0530

    bug in profile display

--- src/citalyser/ui/control/DisplayMaster.java
+++ src/citalyser/ui/control/DisplayMaster.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package citalyser.ui.control;
-   
+
 import citalyser.Main;
 import citalyser.model.Author;
 import citalyser.model.Paper;
@@ -11,6 +11,7 @@ import citalyser.model.PaperCollection;
 import citalyser.model.query.Query;
 import citalyser.model.query.QueryHandler;
 import citalyser.model.query.QueryType;
+import citalyser.model.query.queryresult.AuthorResult;
 import citalyser.ui.control.masters.SearchMaster;
 import citalyser.ui.control.masters.SettingsMaster;
 import citalyser.ui.control.switchers.QueryResultRenderingHandler;
@@ -36,16 +37,13 @@ import org.apache.log4j.Logger;
  * @author Tanmay Patil
  */
 public class DisplayMaster {
-    
+
     private MainFrame mainFrame;
     private javax.swing.JDialog settingsDialog;
     private ExternalPanel extraPanel;
-    
     private SettingsMaster settingsMaster;
     private SearchMaster searchMaster;
-    
     private QueryResultRenderingHandler queryResultRenderingHandler;
-    
     private static Logger logger = Logger.getLogger(DisplayMaster.class.getName());
 
     public DisplayMaster() {
@@ -78,7 +76,7 @@ public class DisplayMaster {
     public QueryResultRenderingHandler getQueryResultRenderingHandler() {
         return queryResultRenderingHandler;
     }
-    
+
     public void searchKeyPressed(SearchPanel searchPanel, char key) {
         searchMaster.searchKeyPressed(searchPanel, key);
     }
@@ -86,7 +84,7 @@ public class DisplayMaster {
     public void settingsButtonPressed() {
         settingsDialog.setVisible(true);
     }
-    
+
     public void mainFrameChanged(int x, int y, int width, int height) {
         if (settingsDialog != null) {
             settingsDialog.setLocation(x, y);
@@ -110,7 +108,7 @@ public class DisplayMaster {
     public void searchButtonClicked(SearchPanel searchPanel) {
         searchMaster.searchButtonClicked(searchPanel);
     }
-    
+
     public void settingsSaveAndClose() {
         settingsDialog.setVisible(false);
     }
@@ -142,7 +140,7 @@ public class DisplayMaster {
     public void addAutoCompleteSuggestions(Vector<String> suggestions) {
         searchMaster.addAutoCompleteSuggestions(suggestions);
     }
-    
+
     public void addAutoCompleteSuggestion(String suggestion) {
         searchMaster.addAutoCompleteSuggestion(suggestion);
     }
@@ -150,38 +148,36 @@ public class DisplayMaster {
     public void emptyAutoCompleteSuggestions() {
         searchMaster.emptyAutoCompleteSuggestions();
     }
-    
-    public boolean checkAuthorMode(){
+
+    public boolean checkAuthorMode() {
         return mainFrame.getRegularDisplayPanel().getHeaderPanel().isAuthorSearchMode();
     }
 
     public void tableClicked(Paper paper) {
-        Query q =new Query.Builder("").flag(QueryType.CITATIONS_LIST).Url(paper.getUrl()).build();
+        Query q = new Query.Builder("").flag(QueryType.CITATIONS_LIST).Url(paper.getUrl()).build();
         System.out.println(q.url);
-        PaperCollection pc = (PaperCollection)QueryHandler.getInstance().getQueryResult(q).getContents();
+        PaperCollection pc = (PaperCollection) QueryHandler.getInstance().getQueryResult(q).getContents();
         if (paper != null) {
-            renderCitationsList(mainFrame.getRegularDisplayPanel().getDataVisualizationPanel().getContentDisplayPanel().getDetailsDisplayPanel().getLowerDetailsDisplayPanel(), pc.getPapers() );
+            renderCitationsList(mainFrame.getRegularDisplayPanel().getDataVisualizationPanel().getContentDisplayPanel().getDetailsDisplayPanel().getLowerDetailsDisplayPanel(), pc.getPapers());
         }
     }
 
     public void authorGridEntityClicked(String id) {
-        
-        final String myId = id;
-        
-        new Thread() {
 
+        final String myId = id;
+
+        new Thread() {
             @Override
             public void run() {
                 queryResultRenderingHandler.render(mainFrame.getRegularDisplayPanel().getDataVisualizationPanel().getContentDisplayPanel().getCentralContentDisplayPanel(), QueryHandler.getInstance().getQueryResult(new Query.Builder("").flag(QueryType.AUTH_PROF).ID(myId).build()));
+                renderProfile(mainFrame.getRegularDisplayPanel().getDataVisualizationPanel().getContentDisplayPanel().getDetailsDisplayPanel().getLowerDetailsDisplayPanel(), ((AuthorResult) QueryHandler.getInstance().getQueryResult(new Query.Builder("").flag(QueryType.AUTH_PROF).ID(myId).build())).getContents());
             }
-            
         }.start();
     }
-    
+
     //*****************************************************************************//
     //**************************** Rendering Functions ****************************//
     //*****************************************************************************//
-
     public void render(ContentRenderer contentRenderer, ArrayList<Author> arrayList) {
         if (arrayList != null) {
             contentRenderer.getGridDisplayPanel().clear();
@@ -221,6 +217,13 @@ public class DisplayMaster {
             Main.getDisplayController().displayErrorMessage("Null Citations List");
         }
     }
-
-
+    
+    public void renderProfile(ContentRenderer contentRenderer, Author author) {
+        if (author != null) {
+            contentRenderer.getProfileDisplayPanel().displayAuthorProfile(author);
+            contentRenderer.flipToProfileDisplayPanel();
+        } else {
+            Main.getDisplayController().displayErrorMessage("Null Author");
+        }
+    }
 }
