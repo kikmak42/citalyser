2773c93351bbb86a75fb5614fbe6bb475671b8fa
milind tahalani <milindtahalani1@gmail.com>
2013-03-23 16:31:14 -0800

    array of nodeObjects created and tested

--- src/citalyser/graph/util/graphData.java
+++ src/citalyser/graph/util/graphData.java
@@ -5,10 +5,13 @@
 package citalyser.graph.util;
 
 import citalyser.dataextraction.parsing.Parser;
+import citalyser.model.Paper;
+import citalyser.model.PaperCollection;
 import citalyser.model.query.queryresult.PaperCollectionResult;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -20,6 +23,12 @@ public class graphData {
     
     static Logger logger = Logger.getLogger(graphData.class.getName());
     static String returnValue;
+    static int id=0;
+
+    public graphData() {
+    this.id =0;
+    }
+    
     public static void main(String args[]){
         PropertyConfigurator.configure("log4j.properties");
 
@@ -45,10 +54,30 @@ public class graphData {
                 }
             }
         }
-        PaperCollectionResult p= new PaperCollectionResult();
-       
+        
+        graphData g = new graphData();
+        for(graphObject o :g.getNodeArray())
+        {
+            logger.debug(o.EntireInfo);
+        }
          
         
+    }
+    
+    public ArrayList<graphObject> getNodeArray(){
+        ArrayList<graphObject> arr = new ArrayList<>();
+        PaperCollection p= new PaperCollection();
+        Parser parse = new Parser();
+        p = (PaperCollection) parse.extractGeneralQuery(returnValue).getContents();
+        for (Paper paper:p.getPapers()){
+            graphObject g = new graphObject();
+            g.Title = paper.getTitle();
+            g.EntireInfo = "<html><head></head><body><B>"+paper.getTitle()+"</B><br>"+paper.getInfo()+"</body></html>";
+            g.id = this.id;
+            this.id+=1;
+            arr.add(g);
+        }              
+        return arr;
     }
 }
 
---
+++ src/citalyser/graph/util/graphObject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citalyser.graph.util;
+
+/**
+ *
+ * @author milindtahalani1
+ */
+public class graphObject {
+    public int id;
+    public String EntireInfo;
+    public String Title;
+
+    public graphObject() {
+        this.id = -1;
+        this.Title = "";
+        this.EntireInfo="";
+    }
+    
+    
+}
